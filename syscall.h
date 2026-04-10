0601934d3fbee283fcc09c587dd1a386c25ada8b
Sai Kaushik S <saikaushik609@gmail.com>
2026-04-10 08:08:15 +0530

    Removing traces of exec

--- syscall.h
+++ syscall.h
@@ -2,8 +2,7 @@
 #define SYS_open    1
 #define SYS_write   2
 #define SYS_close   3
-#define SYS_exec    4
-#define SYS_uptime  5
-#define SYS_sleep   6
-#define SYS_getpid  7
-#define SYS_mknod   8
+#define SYS_uptime  4
+#define SYS_sleep   5
+#define SYS_getpid  6
+#define SYS_mknod   7
